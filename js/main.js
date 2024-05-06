d24723f6b0967f7c34e7ba91127763cdaab4e5a4
LuckyLoo <101459565+LuckyLoo1730@users.noreply.github.com>
2024-05-06 02:47:20 +0100

    Uploaded the website

---
+++ js/main.js
@@ -0,0 +1,93 @@
+anime({
+    targets: '#lunar',
+    color: [
+        { value: '#fc9d03' }, // Orange
+        { value: '#fc7f03' }, // Darker Orange
+        { value: '#fc9d03' }, // Orange
+        { value: '#fc7f03' }  // Darker Orange
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '#wiki',
+    color: [
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '#luckyloo',
+    color: [
+        { value: '#03fc77' }, 
+        { value: '#03fc88' },
+        { value: '#03fc99' }, 
+        { value: '#03fcc6' },
+        { value: '#03fc99' }, 
+        { value: '#03fc99' }, 
+        { value: '#03fc88' },
+
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '#projects',
+    color: [
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '#media',
+    color: [
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
+
+anime({
+    targets: '#motd',
+    color: [
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+        { value: '#fc7f03' },
+        { value: '#fc9d03' },
+    ],
+    loop: true,
+    duration: 4000,
+    easing: 'linear'
+});
